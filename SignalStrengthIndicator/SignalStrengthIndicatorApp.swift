1c58e9eedc63dbb6b659e4d711847209f2ce4d97
Yevgeny <y.yezub@gmail.com>
2023-04-17 09:21:22 +0800

    Initial Commit

---
+++ SignalStrengthIndicator/SignalStrengthIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalStrengthIndicatorApp.swift
+//  SignalStrengthIndicator
+//
+//  Created by Yevgeny Yezub on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SignalStrengthIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
